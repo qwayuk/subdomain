1c21cf28ee89e9f2dc3200dbf0e9cde87eb42a89
qwayuk <70248557+qwayuk@users.noreply.github.com>
2024-04-15 17:21:17 +0800

    Create *.bgp.1bt.uk.js
    Thank you!

---
+++ domains/*.bgp.1bt.uk.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'bgp speed test development',
+  domain: '1bt.uk',
+  subdomain: '*.bgp',
+  owner: {
+    email: '1btuk@1658.eu.org>',
+  },
+  record: {
+    NS: ['achiel.ns.cloudflare.com', 'carlane.ns.cloudflare.com'],
+  },
+  proxy: false
+})
